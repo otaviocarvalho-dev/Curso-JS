1f6475af0d34b64269750abf6266e3900e559055
Otavio Carvalho <otaviocarvalhodonascimento@gmail.com>
2025-03-22 05:10:29 -0300

    Curso-JS-V1.22

---
+++ aula14/aula14ex/ex017/ex017.js
@@ -0,0 +1,22 @@
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    }
+    else if (num.value == 0) {
+        window.alert('Por favor, digite umúmero diferente de 0!')
+    }
+    else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
